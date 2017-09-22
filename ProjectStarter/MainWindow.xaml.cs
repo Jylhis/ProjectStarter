92f5f32af2ecc86e00b6cd7c0715f1566fb3a59e
Jylhis <markus@jylhis.com>
2017-09-22 18:56:24 +0300

    Fix settings

--- ProjectStarter/MainWindow.xaml.cs
+++ ProjectStarter/MainWindow.xaml.cs
@@ -1,21 +1,15 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Shell;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace ProjectStarter
 {
@@ -30,10 +24,20 @@ namespace ProjectStarter
         {
             InitializeComponent();
 
+            string FolderPath = (string)Properties.Settings.Default.ProjectFolder;
+
             // Load projects
-            Projects = Load_Projects("D:\\Projects");
+            Projects = Load_Projects(FolderPath);
             Init_JumpMenu();
             Init_ListView();
+
+            // Checkbox
+            CheckBox check = ((CheckBox)FindName("Settings_Save"));
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if(rk.GetValue("ProjectStarter") != null)
+            {
+                check.IsChecked = true;
+            }
         }
 
         private void Init_ListView()
@@ -56,12 +60,18 @@ namespace ProjectStarter
 
             foreach (var Key in Projects.Keys)
             {
+                JumpPath pat = new JumpPath
+                {
+                    Path = Projects[Key],
+                    CustomCategory = "Projects"
+                };
+
                 JumpTask task = new JumpTask
                 {
                     Title = Key,
                     //Arguments = Projects[Key],
-                    CustomCategory = "Last Opened Projects",
-                    IconResourcePath = Assembly.GetEntryAssembly().CodeBase,
+                    CustomCategory = "Projects",
+                    // IconResourcePath = Assembly.GetEntryAssembly().CodeBase,
                     ApplicationPath = Projects[Key]
                 };
 
@@ -118,6 +128,55 @@ namespace ProjectStarter
             Debug.WriteLine(item.Content.ToString());
             System.Diagnostics.Process.Start(Projects[item.Content.ToString()]);
             System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Edit_Folder_Path(object sender, RoutedEventArgs e)
+        {
+
+            var dlg = new CommonOpenFileDialog();
+            dlg.Title = "My Title";
+            dlg.IsFolderPicker = true;
+            //dlg.InitialDirectory = currentDirectory;
+
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems = false;
+            // dlg.DefaultDirectory = currentDirectory;
+            dlg.EnsureFileExists = true;
+            dlg.EnsurePathExists = true;
+            dlg.EnsureReadOnly = false;
+            dlg.EnsureValidNames = true;
+            dlg.Multiselect = false;
+            dlg.ShowPlacesList = true;
+
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var folder = dlg.FileName;
+
+                // Save Settings
+                Properties.Settings.Default.ProjectFolder = folder;
+                Properties.Settings.Default.Save();
+
+                // Restart
+                System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox check = ((CheckBox)FindName("Settings_Save"));
+
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (check.IsChecked == true)
+            {
+                rk.SetValue("ProjectStarter", System.Reflection.Assembly.GetExecutingAssembly().Location.ToString());
+            }
+            else
+            {
+                rk.DeleteValue("ProjectStarter");
+            }
+
         }
     }
 }
